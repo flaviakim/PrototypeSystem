bdda8db91c1ba0728eaa2b4fe979ccaeb4fb6654
Flavia Brogle <flavia.brogle@fhnw.ch>
2025-08-05 09:28:48 +0200

    Restructure into namespace folders

--- Runtime/Assets/AssetSaver.cs
+++ Runtime/Assets/AssetSaver.cs
@@ -1,23 +1,24 @@
 using System.IO;
 using Newtonsoft.Json;
 using UnityEngine;
-using UnityEngine.Assertions;
 
-public static class AssetSaver {
+namespace Assets {
+    public static class AssetSaver {
 
-    public static string AssetPath => Application.streamingAssetsPath;
+        public static string AssetPath => Application.streamingAssetsPath;
 
-    public static bool TrySaveJson<T>(T jsonItem, string jsonPath, bool isOverwrite) {
-        var jsonFullPath = Path.Combine(AssetPath, jsonPath);
-        if (File.Exists(jsonFullPath) && !isOverwrite) {
-            Debug.LogError($"Json file '{jsonFullPath}' already exists.");
-            return false;
+        public static bool TrySaveJson<T>(T jsonItem, string jsonPath, bool isOverwrite) {
+            var jsonFullPath = Path.Combine(AssetPath, jsonPath);
+            if (File.Exists(jsonFullPath) && !isOverwrite) {
+                Debug.LogError($"Json file '{jsonFullPath}' already exists.");
+                return false;
+            }
+
+            var json = JsonConvert.SerializeObject(jsonItem, Formatting.Indented);
+            File.WriteAllText(jsonFullPath, json);
+            Debug.Log($"Json file saved to '{jsonFullPath}'.");
+            return true;
         }
 
-        var json = JsonConvert.SerializeObject(jsonItem, Formatting.Indented);
-        File.WriteAllText(jsonFullPath, json);
-        Debug.Log($"Json file saved to '{jsonFullPath}'.");
-        return true;
     }
-
 }
--- Runtime/PrototypeSystem/JsonPrototypeCollection.cs
+++ Runtime/PrototypeSystem/JsonPrototypeCollection.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using System.IO;
+using Assets;
 using JetBrains.Annotations;
 using Newtonsoft.Json;
 using UnityEngine;
