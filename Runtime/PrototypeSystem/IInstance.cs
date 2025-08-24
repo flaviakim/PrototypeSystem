0a38d1710da88447a967f263d05f3b4bd861681b
Flavia Brogle <flavia.brogle@protonmail.com>
2025-08-24 18:43:36 +0200

    Cleanup unused code

--- Runtime/PrototypeSystem/IInstance.cs
+++ Runtime/PrototypeSystem/IInstance.cs
@@ -5,39 +5,10 @@ namespace PrototypeSystem {
             where TPrototypeData : IPrototypeData 
             where TInitializationData : IInitializationData {
         public TPrototypeData PrototypeData { get; }
-        // void Initialize(TPrototypeData prototypeData, TInitializationData initializationData);
     }
 
     public abstract class Instance<TPrototypeData, TInitializationData> : IInstance<TPrototypeData, TInitializationData> where TPrototypeData : IPrototypeData where TInitializationData : IInitializationData {
-        // private bool _isInitialized = false;
         public TPrototypeData PrototypeData { get; private set; }
-
-        // protected Instance() { }
-        //
-        // protected Instance(TPrototypeData prototypeData, TInitializationData initializationData) {
-        //     Initialize(prototypeData, initializationData);
-        // }
-        //
-        // public void Initialize(TPrototypeData prototypeData, TInitializationData initializationData) {
-        //     if (_isInitialized) {
-        //         Debug.LogWarning($"Instance {this} is already initialized");
-        //         return;
-        //     }
-        //     PrototypeData = prototypeData;
-        //     Initialize(initializationData);
-        //     _isInitialized = true;
-        // }
-        //
-        // protected abstract void Initialize(TInitializationData initializationData);
     }
-
-    // public static class InstanceExtensions {
-    //     public static string GetIDName<TPrototypeData, TInitializationData>(
-    //         this IInstance<TPrototypeData, TInitializationData> instance)
-    //         where TPrototypeData : IPrototypeData
-    //         where TInitializationData : IInitializationData {
-    //         return instance.PrototypeData.IDName;
-    //     }
-    // }
 
 }
