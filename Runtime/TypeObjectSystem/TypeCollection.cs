088fe612e934ce1e6076acba9503574ca654fcb6
Flavia Brogle <flavia.brogle@protonmail.com>
2025-09-14 10:58:08 +0200

    Change signature of loading types in Type Collection

--- Runtime/TypeObjectSystem/TypeCollection.cs
+++ Runtime/TypeObjectSystem/TypeCollection.cs
@@ -7,29 +7,14 @@ using Debug = UnityEngine.Debug;
 namespace TypeObjectSystem {
     public class TypeCollection<TType> : ITypeCollection<TType> where TType : IType {
         private readonly Dictionary<string, TType> _types = new(StringComparer.OrdinalIgnoreCase);
-        
-        private ITypeLoader<TType> _typeLoader;
-        public ITypeLoader<TType> TypeLoader {
-            get => _typeLoader;
-            set {
-                if (value == null) {
-                    Debug.LogError($"typeLoader is null for TypeCollection of {typeof(TType).Name}.");
-                    return;
-                }
-                _typeLoader = value;
-            }
+
+        public TypeCollection() {
         }
 
-        public TypeCollection(ITypeLoader<TType> typeLoader, bool preloadTypes = true) {
-            if (typeLoader == null) {
-                Debug.LogError($"typeLoader is null for TypeCollection of {typeof(TType).Name}.");
-            }
-            TypeLoader = typeLoader;
-            if (preloadTypes) {
-                PreloadAll();
-            }
+        public TypeCollection(params ITypeLoader<TType>[] typeLoaders) {
+            LoadTypes(typeLoaders: typeLoaders);
         }
-        
+
         public bool TryGetTypeForName(string idName, out TType type) {
             CheckForErrors(idName);
             return _types.TryGetValue(idName, out type);
@@ -44,36 +29,44 @@ namespace TypeObjectSystem {
             CheckIsInitialized();
             return _types.Values;
         }
-        
-        public void PreloadAll() {
-            if (TypeLoader == null) {
-                Debug.LogError($"typeLoader is null for TypeCollection of {typeof(TType).Name}.");
+
+        public void LoadTypes(bool clearExisting = true, params ITypeLoader<TType>[] typeLoaders) {
+            if (typeLoaders == null) {
+                Debug.LogError($"{nameof(LoadTypes)} called with null {nameof(typeLoaders)}");
                 return;
             }
-            _types.Clear();
-            Dictionary<string, TType> typeData = TypeLoader.LoadAll();
-            foreach (KeyValuePair<string, TType> pair in typeData) {
-                _types.Add(pair.Key, pair.Value);
+
+            if (clearExisting) {
+                _types.Clear();
+            }
+
+            foreach (ITypeLoader<TType> typeLoader in typeLoaders) {
+                Dictionary<string, TType> typeData = typeLoader.LoadAll();
+                foreach (KeyValuePair<string, TType> pair in typeData) {
+                    _types.Add(pair.Key, pair.Value);
+                }
             }
         }
-        
+
         public void AddOrReplace(TType def) {
             if (def == null || string.IsNullOrEmpty(def.IDName)) {
                 Debug.LogError($"Cannot add null or invalid type to collection of {typeof(TType).Name}.");
                 return;
             }
+
             _types[def.IDName] = def;
         }
 
         public void Clear() {
             _types.Clear();
         }
-        
+
         [Conditional("DEBUG")]
         private void CheckForErrors(string idName) {
             if (string.IsNullOrEmpty(idName)) {
                 Debug.LogError("idName is null or empty.");
             }
+
             CheckIsInitialized();
         }
 
@@ -83,6 +76,5 @@ namespace TypeObjectSystem {
                 Debug.LogError($"Type collection for {typeof(TType).Name} is empty.");
             }
         }
-
     }
 }
